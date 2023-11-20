d8155f8d15c4f60c567ea85aeffc30b7aad18be2
Asif Matin <asifmatin@hotmail.com>
2023-11-20 14:41:55 -0500

    update java version 1.0.0

---
+++ src/test/java/ui/DemoAutomation2.java
@@ -0,0 +1,21 @@
+package ui;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoAutomation2 {
+
+	public static void main(String[] args) {
+
+		//System.setProperty("webdriver.chrome.driver", "C:\\browser_drivers\\chrome.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+		//driver.get("http://www.google.com");
+		driver.get("http://www.ebay.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("mobile");
+		driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).click();
+		driver.close();
+		
+	}
+
+}
